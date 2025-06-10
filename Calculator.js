d61e9ca64499a6e9fe38e6cecb6b044a4b415487
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-06-10 16:04:10 -0700

    finished javascript and got calculator working jun 10, 2025

--- Calculator.js
+++ Calculator.js
@@ -1,72 +1,93 @@
-// CALCULATOR VARIABLES 
+// VARIABLES
+let currentInput = "";
+let prevInput = "";
+let operator = "";
 let num1 = 0;
 let num2 = 0;
-let operator;
 
-// MAKE FUNCTIONS FOR MATH OPERATIONS
-function add(num1, num2) {
-    let value = num1 + num2;
-    return value;
+function add(num1,num2) {
+    let sum = num1 + num2;
+    return sum;
 }
 
-function subtract(num1, num2) {
-    let value = num1 - num2;
-    return value;
+function multiply(num1,num2) {
+    let sum = num1 * num2;
+    return sum;
 }
 
-function multiply(num1, num2) {
-    let value = num1 * num2;
-    return value;
+function subtract(num1,num2) {
+    let sum = num1 - num2;
+    return sum;
 }
 
-function divide(num1, num2) {
-    let value = num1 / num2;
-    return value;
+function divide(num1,num2) {
+    let sum = num1 / num2;
+    return sum;
 }
 
-// TEST FUNCTIONS IN CONSOLE
+function modulo(num1,num2) {
+    let remainder = num1 % num2;
+    return remainder;
+}
 
-// ADD
-console.log(add(2,5));
-console.log(add(5,5));
-console.log(add(20,5));
-console.log(add(25,50));
-console.log(add(200,5000));;
+const display = document.querySelector(".screen-part");
 
-// SUBTRACT
-console.log(subtract(12,5));
-console.log(subtract(50,5));
-console.log(subtract(20,5));
-console.log(subtract(250,50));
-console.log(subtract(20000,5000));;
-
-// MULTIPLY
-console.log(multiply(2,5));
-console.log(multiply(5,5));
-console.log(multiply(20,5));
-console.log(multiply(25,50));
-console.log(multiply(200,5000));
-
-// DIVIDE
-console.log(divide(236,5));
-console.log(divide(5,5));
-console.log(divide(20,3));
-console.log(divide(50,25));
-console.log(divide(200,5));
-
-// OPERATE FUNCTION 
-function operate(num1, num2, operator) {
-    // FIX ME: NEED TO ADD OUT PUT TO CALC SCREEN AFTER BUILDING CALC WITH HTML/CSS
-    if (operator == '+') {
-        add(num1,num2);
+function appendToDisplay(input) {
+    if (input === '+' || input === '-' || input === 'x' || input === '/' || input === '%') {
+        display.innerText += input;
+        operator = input
+        prevInput = currentInput;
+        currentInput = ""
     }
-    else if (operator == '-') {
-        subtract(num1,num2);
+    else {
+        display.innerText += input;
+        currentInput += input;
     }
-    else if (operator == '*') {
-        multiply(num1,num2);
-    }
-    else if (operator == '/') {
-        divide(num1,num2);
+}
+
+function clearDisplay() {
+    display.innerText = "";
+    currentInput = "";
+    prevInput = "";
+    operator = "";
+}
+
+function calculate() {
+    switch (operator) {
+        case '+':
+            num1 = parseFloat(prevInput);
+            num2 = parseFloat(currentInput);
+             clearDisplay()
+             currentInput = add(num1,num2);
+             display.innerText = currentInput;
+            break;
+        case '-':
+            num1 = parseFloat(prevInput);
+            num2 = parseFloat(currentInput);
+            clearDisplay()
+            currentInput = subtract(num1,num2);
+            display.innerText = currentInput;
+            break;
+        case 'x':
+            num1 = parseFloat(prevInput);
+            num2 = parseFloat(currentInput);
+            clearDisplay()
+            currentInput = multiply(num1,num2);
+            display.innerText = currentInput;
+            break;
+        case '/':
+            num1 = parseFloat(prevInput);
+            num2 = parseFloat(currentInput);
+            clearDisplay()
+            currentInput = divide(num1,num2);
+            display.innerText = currentInput;
+            break;
+        case '%':
+            num1 = parseFloat(prevInput);
+            num2 = parseFloat(currentInput);
+            clearDisplay()
+            currentInput = modulo(num1,num2);
+            display.innerText = currentInput;
+            break;
     }
 }
